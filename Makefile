c55423c7c1c2a5dcd6661c8451f35790f015649d
nowislewis <1226228799@qq.com>
2022-05-07 11:35:47 +0800

    update update_submodule.sh

--- Makefile
+++ Makefile
@@ -10,7 +10,6 @@ bootstrap-borg:
 clean-init: tangle-init
 # 更新到 remote 最新 commit
 update:
-	#git submodule foreach git pull
 	./etc/update_submodule.sh
 # 初始化下载，更新到 .gitmodules 中指定的 commit
 init:
--- etc/update_submodule.sh
+++ etc/update_submodule.sh
@@ -1,19 +1,13 @@
-
-FN=/tmp/emacs-update-sessions
+log_file=/tmp/git_update_log
+rm $log_file
+touch $log_file
 
 function pull() {
-     FN=/tmp/emacs-update-sessions
-     touch $FN
-     PN=$(basename $(pwd))
-     CNT=$(grep -c $PN /tmp/emacs-update-sessions)
-     echo $PN $CNT
-     if [ $CNT -ge 1 ]; then
-	echo skip
-     else
-        git checkout master || git checkout main; git pull && echo $PN >> $FN;
-     fi 
+    log_file=/tmp/git_update_log
+    PN=$(basename $(pwd))
+    last_commit=$(git rev-parse HEAD)
+        git checkout master || git checkout main; echo $PN >> $log_file&&git pull; git log $last_commit...HEAD --no-merges --color --graph --date=format:'%Y-%m-%d %H:%M:%S' --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Cblue %s %sCgreen(%cd) %C(bold blue)<%an>%Creset' --abbrev-commit >>$log_file;
 }
 
 export -f pull
-
-git submodule foreach 'pull' && rm $FN
+git submodule foreach 'pull' && cat $log_file
