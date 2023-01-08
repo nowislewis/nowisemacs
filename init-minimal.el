33987b70225a59fc5c28955048ecaba12ac0f3c1
nowislewis <1226228799@qq.com>
2023-01-08 18:32:34 +0800

    add a full-gtd method

--- init-minimal.el
+++ init-minimal.el
@@ -1,4 +1,21 @@
-;; -*- lexical-binding: t; -*-
+  (setq package-enable-at-startup nil)
+
+  ;; Inhibit resizing frame
+  (setq frame-inhibit-implied-resize t)
+
+  ;; no title bar
+  ;; (add-to-list 'default-frame-alist '(undecorated . t))
+  ;; Faster to disable these here (before they've been initialized)
+  (push '(alpha-background . 85) default-frame-alist)
+  (push '(menu-bar-lines . 0) default-frame-alist)
+  (push '(tool-bar-lines . 0) default-frame-alist)
+  (push '(vertical-scroll-bars) default-frame-alist)
+  ;; (push '(undecorated . t) default-frame-alist)
+  (setq initial-major-mode 'fundamental-mode ;; 默认用最简单的模式
+        package--init-file-ensured t
+        inhibit-startup-message t             ; 关闭启动 Emacs 时的欢迎界面
+        )
+
 (icomplete-vertical-mode)
 (setq use-short-answers t)
 (setq confirm-kill-emacs 'y-or-n-p)
