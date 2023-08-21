15ebf8009f327852807d71d29d582f2ddd3310ce
lewisliu <1226228799@qq.com>
2023-08-21 23:44:25 +0800

    add org-fc with keymap, update submodule utils

--- useful-tools/update_submodule.sh
+++ useful-tools/update_submodule.sh
@@ -6,7 +6,7 @@ function pull() {
     log_file=/tmp/git_update_log
     PN=$(basename $(pwd))
     last_commit=$(git rev-parse HEAD)
-        git checkout master || git checkout main; echo "package:    $PN" >> $log_file&&git pull; git log $last_commit...HEAD --no-merges --color --graph --date=format:'%Y-%m-%d %H:%M:%S' --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Cblue %s %sCgreen(%cd) %C(bold blue)<%an>%Creset' --abbrev-commit >>$log_file;echo "\n">>$log_file;
+        echo "package:    $PN" >> $log_file&&git pull --rebase; git log $last_commit...HEAD --no-merges --color --graph --date=format:'%Y-%m-%d %H:%M:%S' --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Cblue %s %sCgreen(%cd) %C(bold blue)<%an>%Creset' --abbrev-commit >>$log_file;echo "\n">>$log_file;
 }
 
 export -f pull
