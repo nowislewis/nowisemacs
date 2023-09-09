cc1d5b2a92da1672bb2bd90dc7b21c2457a7b9a2
lewisliu <1226228799@qq.com>
2023-09-09 17:45:15 +0800

    update packages, add visual-line-mode in prog and org-mode, change rime to submodule of my emacs doc

--- useful-tools/update_submodule.sh
+++ useful-tools/update_submodule.sh
@@ -3,10 +3,13 @@ rm $log_file
 touch $log_file
 
 function pull() {
-    log_file=/tmp/git_update_log
-    PN=$(basename $(pwd))
-    last_commit=$(git rev-parse HEAD)
-        echo "package:    $PN" >> $log_file&&git reset --hard&&git pull --rebase; git log $last_commit...HEAD --no-merges --color --graph --date=format:'%Y-%m-%d %H:%M:%S' --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Cblue %s %sCgreen(%cd) %C(bold blue)<%an>%Creset' --abbrev-commit >>$log_file;echo "\n">>$log_file;
+	log_file=/tmp/git_update_log
+	PN=$(basename $(pwd))
+	last_commit=$(git rev-parse HEAD)
+	echo "package:    $PN" >>$log_file
+	git reset --hard && git pull
+	git log $last_commit...HEAD --no-merges --color --graph --date=format:'%Y-%m-%d %H:%M:%S' --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Cblue %sCgreen(%cd) %C(bold blue)<%an>%Creset' --abbrev-commit >>$log_file
+	echo "\n" >>$log_file
 }
 
 export -f pull
