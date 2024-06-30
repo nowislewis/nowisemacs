0ba2c9de070e17bf7828b021caf237bf14f06ecc
lewisliu <1226228799@qq.com>
2024-06-30 21:49:47 +0800

    update readme and make update configs, disable treesit-auto by default

--- useful-tools/update_submodule.sh
+++ useful-tools/update_submodule.sh
@@ -5,11 +5,13 @@ touch $log_file
 function pull() {
 	log_file=/tmp/git_update_log
 	PN=$(basename $(pwd))
+    git checkout main >/dev/null 2>&1||git checkout master >/dev/null 2>&1
 	last_commit=$(git rev-parse HEAD)
 	echo "package:    $PN" >>$log_file
-	git reset --hard && git pull
+	git reset --hard  && git pull
 	git log $last_commit...HEAD --no-merges --color --graph --date=format:'%Y-%m-%d %H:%M:%S' --pretty=format:'%C(red)%h%Creset -%C(green)(%cd) %C(yellow)%d%C(blue)  %Creset%s %C(bold blue)<%an>%Creset' --abbrev-commit >>$log_file
-	echo "\n" >>$log_file
+	echo $'\n' >>$log_file
+	echo $'\n'
 }
 
 export -f pull
